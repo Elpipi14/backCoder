ae59fa3550526518d83ddea2548a6ac2ddf1cf48
Andy <ands_riss@hotmail.com>
2024-04-28 22:07:10 -0300

    jwt

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -9,33 +9,33 @@ const JWTStrategy = jwt.Strategy;
 const ExtractJwt = jwt.ExtractJwt;
 
 const cookieExtractor = (req) => {
-    let token = null;
-    if (req && req.cookies) {
-        token = req.cookies["coderHouseToken"];
-    }
-    return token;
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies["coderHouseToken"];
+  }
+  return token;
 }
 
 const initializePassport = () => {
 
-    const jwtExtractor = {
-        jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
-        secretOrKey: JWT_SECRET,
-      };
-    
-    passport.use("jwt", new JWTStrategy(jwtExtractor , async (jwt_payload, done) => {
-        try {
-            console.log(`busca el usuario passport: `, jwt_payload);
-            const user = await UserModel.findById(jwt_payload.user._id);
-            console.log(`busca despues playLoad: `, user);
-            if (!user) {
-                return done(null, false);
-              }
-              return done(null, user);
-            } catch (error) {
-              return done(error, false);
-            }
-    }));
+  const jwtExtractor = {
+    jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
+    secretOrKey: JWT_SECRET,
+  };
+
+  passport.use("jwt", new JWTStrategy(jwtExtractor, async (jwt_payload, done) => {
+    try {
+      // console.log(`busca el usuario passport: `, jwt_payload);
+      const user = await UserModel.findById(jwt_payload.user._id);
+      // console.log(`busca despues playLoad: `, user);
+      if (!user) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error, false);
+    }
+  }));
 };
 
 export default initializePassport;
--- src/routes/views.js
+++ src/routes/views.js
@@ -1,18 +1,17 @@
 import { Router } from "express";
+import passport from "passport";
 
 import ChatManager from "../daos/mongoDb/DB/chat.manager.js";
 import ProductsManager from "../daos/mongoDb/DB/productsManager.js";
-import UserManager from "../daos/mongoDb/DB/userManager.js";
+import CartsManager from "../daos/mongoDb/DB/carts.Manager.js";
 
-import passport from "passport";
-import generationToken from "../utils/jwt.js";
 
 const routerViews = Router();
 const productsDB = new ProductsManager();
 const chatManager = new ChatManager();
-const userManger = new UserManager();
+const cartManager = new CartsManager();
 
-routerViews.get('/', passport.authenticate("jwt", {session:false, failureRedirect: "/login" }), async (req, res) => {
+routerViews.get('/', passport.authenticate("jwt", { session: false, failureRedirect: "/login" }), async (req, res) => {
     try {
         const productList = await productsDB.getAll()
         const leanProducts = productList.payload.products.map(product => product.toObject({ getters: true }))
@@ -23,7 +22,7 @@ routerViews.get('/', passport.authenticate("jwt", {session:false, failureRedirec
     }
 });
 
-routerViews.get('/products', passport.authenticate("jwt", {session:false, failureRedirect: "/login" }), async (req, res) => {
+routerViews.get('/products', passport.authenticate("jwt", { session: false, failureRedirect: "/login" }), async (req, res) => {
     try {
         const page = req.query.page || 1;
         const productList = await productsDB.getAll(page);
@@ -50,6 +49,74 @@ routerViews.get('/view/:id', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+
+// Carts
+routerViews.get('/cart', passport.authenticate("jwt", { session: false }), async (req, res) => {
+    try {
+        // Verificar si el usuario está autenticado y tiene un carrito asociado
+        if (req.isAuthenticated() && req.user.cartId) {
+            // Obtener el ID del carrito del usuario autenticado
+            const cartId = req.user.cartId;
+            // Obtener los productos del carrito utilizando el ID del carrito
+            const cart = await cartManager.getById(cartId);
+            const products = cart.products;
+
+            // Crear un mapa para almacenar los productos agrupados por su ID
+            const productMap = new Map();
+
+            // Iterar sobre cada producto y agregarlo al mapa
+            for (const item of products) {
+                const productId = item.product;
+                const productDetails = await productsDB.getById(productId);
+                if (productMap.has(productId)) {
+                    // Si el producto ya está en el mapa, sumar la cantidad
+                    const existingProduct = productMap.get(productId);
+                    existingProduct.quantity += item.quantity;
+                } else {
+                    // Si el producto no está en el mapa, agregarlo
+                    productMap.set(productId, {
+                        product: productDetails,
+                        quantity: item.quantity,
+                        _id: item._id
+                    });
+                }
+            }
+
+            // Convertir el mapa de productos a un array
+            const productsWithDetails = Array.from(productMap.values());
+
+            // Renderizar la vista del carrito pasando los productos con sus detalles
+            res.render('partials/cart', { products: productsWithDetails });
+        } else {
+            // Si el usuario no está autenticado o no tiene un carrito asociado, renderizar el carrito vacío
+            res.render('partials/cart', { products: [] });
+        }
+    } catch (error) {
+        console.error('Error al obtener productos del carrito:', error.message);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+
+// Ruta para agregar un producto al carrito
+routerViews.post('/cart/add/:productId', passport.authenticate("jwt", { session: false }), async (req, res) => {
+    try {
+        // Obtener el ID del producto
+        const productId = req.params.productId;
+        // Obtener el ID del carrito asignado al usuario desde el objeto de usuario
+        const cartId = req.user.cartId;
+        // Agregar el producto al carrito del usuario utilizando el ID del carrito asignado
+        await cartManager.addToCart(cartId, productId);
+
+        res.redirect('/cart'); // Redirigir al usuario al carrito después de agregar el producto
+    } catch (error) {
+        console.error('Error adding product to cart:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
 
 ///-------chat´s---------///
 
